409315bb93b83887cae37bbb778f8e41f6b7a0cf
Maggie Pierse <margaretpierse@Margaret-Pierses-MacBook-Pro.local>
2023-10-02 09:40:28 -0400

    chapter 8 complete

---
+++ chapter8/math/math.go
@@ -0,0 +1,31 @@
+package math
+
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+
+	return total / float64(len(xs))
+}
+
+func Min(xs []float64) float64 {
+	min := xs[0]
+	for _, x := range xs {
+		if x < min {
+			min = x
+		}
+	}
+
+	return min
+}
+
+func Max(xs []float64) float64 {
+	max := xs[0]
+	for _, x := range xs {
+		if x > max {
+			max = x
+		}
+	}
+	return max
+}
